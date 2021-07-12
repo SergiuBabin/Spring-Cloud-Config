b18ec9687c386f22656c6577f999dcde4081245e
Sergiu Babin <48562108+SergiuBabin@users.noreply.github.com>
2021-07-12 16:37:22 +0300

    Finall client

--- Curs2-CloudEureka/zip (2)/src/main/java/Eureka/Client/ITSS/Application.java
+++ Curs2-CloudEureka/zip (2)/src/main/java/Eureka/Client/ITSS/Application.java
@@ -1,10 +1,5 @@
 package Eureka.Client.ITSS;
 
-import com.netflix.discovery.EurekaClient;
-import org.hibernate.validator.internal.util.stereotypes.Lazy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -14,11 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @RestController
 public class Application implements GreetingController {
-
-	@Qualifier("eurekaClient")
-	@Autowired
-	@Lazy
-	private EurekaClient eurekaClient;
 
 
 	public static void main(String[] args) {
