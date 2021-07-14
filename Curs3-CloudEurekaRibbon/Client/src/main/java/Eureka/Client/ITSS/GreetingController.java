99b11b2f7d9cf2a4b3d396069929cca37f928439
Sergiu Babin <48562108+SergiuBabin@users.noreply.github.com>
2021-07-14 14:30:54 +0300

    Ribbon Client finall

--- Curs3-CloudEurekaRibbon/Client/src/main/java/Eureka/Client/ITSS/GreetingController.java
+++ Curs3-CloudEurekaRibbon/Client/src/main/java/Eureka/Client/ITSS/GreetingController.java
@@ -1,12 +1,24 @@
 package Eureka.Client.ITSS;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Babin Sergiu
  */
 
-public interface GreetingController {
-    @RequestMapping("/greeting")
-    String greeting();
+@RestController
+public class GreetingController {
+
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/hello")
+    public String greeting() {
+        return String.format(
+                "Hello from '%d'!", port);
+    }
+
 }
