836fe9d612d8d385c0f8307a6a606fd837e32035
Sergiu Babin <48562108+SergiuBabin@users.noreply.github.com>
2021-07-14 14:30:54 +0300

    Ribbon Client finall

--- Curs3-CloudEurekaRibbon/Client/src/main/java/Eureka/Client/ITSS/Application.java
+++ Curs3-CloudEurekaRibbon/Client/src/main/java/Eureka/Client/ITSS/Application.java
@@ -3,21 +3,15 @@ package Eureka.Client.ITSS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
-public class Application implements GreetingController {
-
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	public String greeting() {
-		return String.format(
-				"Hello from '%s'!");
-	}
+
+
 }
